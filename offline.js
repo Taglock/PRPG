493dc2af012c8f3a7a31b3f0f9796f53bc3afc4c
Taglock <klimjiro@gmail.com>
2020-06-18 01:29:06 +0500

    0.1

--- offline.js
+++ offline.js
@@ -1,17 +1,24 @@
 ﻿{
-	"version": 1592406029,
+	"version": 1592425671,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-3.4.1.min.js",
 		"offlineClient.js",
 		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
 		"media/sss.m4a",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
 		"icon-128.png",
 		"icon-256.png",
-		"loading-logo.png"
+		"loading-logo.png",
+		"data.txt",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js",
+		"adapter.js"
 	]
 }
