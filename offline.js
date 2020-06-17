844b4e313b487d4dd17f2e756c525d8a0750cc19
Taglock <klimjiro@gmail.com>
2020-06-17 19:40:04 +0500

    Initial commit

---
+++ offline.js
@@ -0,0 +1,17 @@
+﻿{
+	"version": 1592399660,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"media/sss.m4a",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
